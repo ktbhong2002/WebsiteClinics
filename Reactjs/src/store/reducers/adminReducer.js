418b976e83bebdaab26282917bef0a94512e85d6
ktbhong2002 <kieuhong05102002@gmail.com>
2024-03-06 23:10:29 +0700

    update-continue

--- Reactjs/src/store/reducers/adminReducer.js
+++ Reactjs/src/store/reducers/adminReducer.js
@@ -4,24 +4,46 @@ const initialState = {
   genders: [],
   roles: [],
   positions: [],
+  isLoadingGender: false,
 };
 
 const adminReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_GENDER_START:
-      console.log("fetch gender start: ", action);
-      return {
-        ...state,
-      };
-    case actionTypes.FETCH_GENDER_SUCCESS:
       let copyState = { ...state };
-      copyState.genders = action.data;
-      console.log("fetch gender success: ", copyState);
+      copyState.isLoadingGender = true;
       return {
         ...copyState,
       };
+    case actionTypes.FETCH_GENDER_SUCCESS:
+      state.genders = action.data;
+      state.isLoadingGender = false;
+      return {
+        ...state,
+      };
     case actionTypes.FETCH_GENDER_FAILDED:
-      console.log("fetch gender faild: ", action);
+      state.isLoadingGender = false;
+      state.genders = [];
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_POSITION_SUCCESS:
+      state.positions = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_POSITION_FAILDED:
+      state.positions = [];
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_ROLE_SUCCESS:
+      state.roles = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_ROLE_FAILDED:
+      state.roles = [];
       return {
         ...state,
       };
