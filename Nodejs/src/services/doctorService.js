acf91255bbeca902038606ab3b9fa10049a579e1
ktbhong2002 <kieuhong05102002@gmail.com>
2024-03-13 18:45:54 +0700

    update

--- Nodejs/src/services/doctorService.js
+++ Nodejs/src/services/doctorService.js
@@ -60,19 +60,36 @@ let saveDetailInforDoctor = (inputData) => {
       if (
         !inputData.doctorId ||
         !inputData.contentHTML ||
-        !inputData.contentMarkdown
+        !inputData.contentMarkdown ||
+        !inputData.action
       ) {
         resolve({
           errCode: 1,
           errMessage: "Missing parameter",
         });
       } else {
-        await db.Markdown.create({
-          contentHTML: inputData.contentHTML,
-          contentMarkdown: inputData.contentMarkdown,
-          description: inputData.description,
-          doctorId: inputData.doctorId,
-        });
+        if (inputData.action === `CREATE`) {
+          await db.Markdown.create({
+            contentHTML: inputData.contentHTML,
+            contentMarkdown: inputData.contentMarkdown,
+            description: inputData.description,
+            doctorId: inputData.doctorId,
+          });
+        }
+        if (inputData.action === `EDIT`) {
+          let doctorMarkdown = await db.Markdown.findOne({
+            where: { doctorId: inputData.doctorId },
+            //raw: true,
+            raw: false,
+          });
+          if (doctorMarkdown) {
+            doctorMarkdown.contentHTML = inputData.contentHTML;
+            doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
+            doctorMarkdown.description = inputData.description;
+            // doctorMarkdown.updateAt = new Date();
+            await doctorMarkdown.save();
+          }
+        }
         resolve({
           errCode: 0,
           errMessage: "Save information doctor successfly",
@@ -113,7 +130,7 @@ let getDetailDoctorById = (inputId) => {
           nest: true,
         });
         if (data && data.image) {
-          data.image = new Buffer(data.image, "base64").toString("binary");
+          data.image = new Buffer.from(data.image, "base64").toString("binary");
         }
         if (!data) data = {};
         resolve({
