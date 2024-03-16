fdcd7de700f6b4716bfb741c7d75a49bcb40cede
ktbhong2002 <kieuhong05102002@gmail.com>
2024-03-16 12:37:58 +0700

    update

--- Reactjs/src/containers/Patient/Doctor/DoctorSchedule.js
+++ Reactjs/src/containers/Patient/Doctor/DoctorSchedule.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./DoctorSchedule.scss";
-import moment from "moment";
+import moment, { lang } from "moment";
 import localization from "moment/locale/vi";
 import { LANGUAGES } from "../../../utils";
 import { getScheduleDoctorByDate } from "../../../services/userService";
@@ -11,6 +11,7 @@ class DoctorSchedule extends Component {
     super(props);
     this.state = {
       allDays: [],
+      allAvailableTime: [],
     };
   }
 
@@ -24,12 +25,17 @@ class DoctorSchedule extends Component {
     this.setArrDays(language);
   }
 
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   setArrDays = (language) => {
     let allDays = [];
     for (let i = 0; i < 7; i++) {
       let object = {};
       if (language === LANGUAGES.VI) {
-        object.label = moment(new Date()).add(i, "days").format("dddd - DD/MM");
+        let labelVi = moment(new Date()).add(i, "days").format("dddd - DD/MM");
+        object.label = this.capitalizeFirstLetter(labelVi);
       } else {
         object.label = moment(new Date())
           .add(i, "days")
@@ -56,12 +62,18 @@ class DoctorSchedule extends Component {
       let doctorId = this.props.doctorIdFromParent;
       let date = event.target.value;
       let res = await getScheduleDoctorByDate(doctorId, date);
+      if (res && res.errCode === 0) {
+        this.setState({
+          allAvailableTime: res.data ? res.data : [],
+        });
+      }
       console.log("check res schedule react: ", res);
     }
   };
 
   render() {
-    let { allDays } = this.state;
+    let { allDays, allAvailableTime } = this.state;
+    let { language } = this.props;
     return (
       <div className="doctor-schedule-container">
         <div className="all-schedule">
@@ -77,7 +89,26 @@ class DoctorSchedule extends Component {
               })}
           </select>
         </div>
-        <div className="all-available-time"></div>
+        <div className="all-available-time">
+          <div className="text-calendar">
+            <i className="fas fa-calendar-alt">
+              <span>Lịch khám</span>
+            </i>
+          </div>
+          <div className="time-content">
+            {allAvailableTime && allAvailableTime.length > 0 ? (
+              allAvailableTime.map((item, index) => {
+                let timeDisplay =
+                  language === LANGUAGES.VI
+                    ? item.timeTypeData.valueVi
+                    : item.timeTypeData.valueEn;
+                return <button key={index}>{timeDisplay}</button>;
+              })
+            ) : (
+              <div>Không có lịch hẹn trong thời gian này!</div>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
