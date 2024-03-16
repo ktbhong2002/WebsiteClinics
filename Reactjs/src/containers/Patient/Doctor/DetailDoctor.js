4088cafe5a0c23a74fa16f65fa82476e39e86591
ktbhong2002 <kieuhong05102002@gmail.com>
2024-03-16 11:28:24 +0700

    update

--- Reactjs/src/containers/Patient/Doctor/DetailDoctor.js
+++ Reactjs/src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,6 +4,7 @@ import HomeHeader from "../../HomePage/HomeHeader";
 import "./DetailDoctor.scss";
 import { getDetailInforDoctor } from "../../../services/userService";
 import { LANGUAGES } from "../../../utils";
+import DoctorSchedule from "./DoctorSchedule";
 
 class DetailDoctor extends Component {
   constructor(props) {
@@ -70,7 +71,17 @@ class DetailDoctor extends Component {
               </div>
             </div>
           </div>
-          <div className="schedule-doctor"></div>
+          <div className="schedule-doctor">
+            <div className="content-left">
+              <DoctorSchedule
+                doctorIdFromParent={
+                  detailDoctor && detailDoctor.id ? detailDoctor.id : -1
+                }
+              />
+            </div>
+            <div className="content-right"></div>
+          </div>
+
           <div className="detail-infor-doctor">
             {detailDoctor &&
               detailDoctor.Markdown &&
@@ -82,6 +93,7 @@ class DetailDoctor extends Component {
                 ></div>
               )}
           </div>
+
           <div className="comment-doctor"></div>
         </div>
       </>
