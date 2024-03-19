4f6b0597febaaef9468dfcbbc3f335c673946a07
ktbhong2002 <kieuhong05102002@gmail.com>
2024-03-19 17:19:42 +0700

    update

--- Nodejs/src/controllers/specialtyController.js
+++ Nodejs/src/controllers/specialtyController.js
@@ -12,6 +12,20 @@ let createSpecialty = async (req, res) => {
     });
   }
 };
+
+let getAllSpecialty = async (req, res) => {
+  try {
+    let infor = await SpecialtyService.getAllSpecialty();
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error form service...",
+    });
+  }
+};
 module.exports = {
   createSpecialty: createSpecialty,
+  getAllSpecialty: getAllSpecialty,
 };
